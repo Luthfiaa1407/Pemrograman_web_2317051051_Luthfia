cb6f843531e89d627f078ff78e40e0a58cb55132
Luthfiaa1407 <luthfiarahmasholihah@gmail.com>
2025-05-08 15:26:58 +0700

    PERTEMUAN 11 php database

---
+++ Pertemuan 11/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Input</title>
+</head>
+<body>
+    <form method="post" action="tampil.php">
+        <label>Masukkan Nama</label><br>
+        <input type="text" name="nama"><br><br>
+
+        <label>Masukkan Usia</label><br>
+        <input type="text" name="usia"><br><br>
+
+        <label>Masukkan NPM</label><br>
+        <input type="text" name="npm"><br><br>
+
+        <label>Masukkan Alamat</label><br>
+        <input type="text" name="alamat"><br><br>
+
+        <input type="submit" value="Kirim">
+    </form>
+</body>
+</html>
